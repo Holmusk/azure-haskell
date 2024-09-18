d6eaddc3f794cbcf97dc17a02388e5cbe2f42cdd
Nitin Prakash <prakash.nitin63@gmail.com>
2024-09-18 15:58:06 +0530

    Remove arbitrary 20 second expiry check

--- azure-auth/Azure/Utils.hs
+++ azure-auth/Azure/Utils.hs
@@ -1,6 +1,6 @@
 module Azure.Utils where
 
-import Data.Time (addUTCTime, getCurrentTime, secondsToNominalDiffTime)
+import Data.Time (getCurrentTime, secondsToNominalDiffTime)
 import Data.Time.Clock.POSIX (posixSecondsToUTCTime)
 import UnliftIO (MonadIO (..))
 
@@ -9,15 +9,13 @@ import Azure.Types (ExpiresOn)
 import qualified Data.Text as Text
 import qualified Text.Read as Text
 
-{- | Check if an azure access token expiration time
-is past or < 20 seconds from current time
--}
+-- | Check if an azure access token has expired
 isExpired :: MonadIO m => ExpiresOn -> m Bool
 isExpired expiresOn = do
     let timestamp = posixSecondsToUTCTime . secondsToNominalDiffTime <$> Text.readMaybe (Text.unpack expiresOn)
     case timestamp of
         Just time -> do
             currentTime <- liftIO getCurrentTime
-            return $ time <= addUTCTime 20 currentTime
+            return $ time <= currentTime
         Nothing ->
             return False
